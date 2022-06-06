3e82573dfdecdcb2e10a6141c54bef162c0514a8
Remo Geissbühler <joe@biden.com>
2022-06-06 23:51:03 +0200

    helper script update & fix

--- server/src/dbHelperScripts/insertDates.ts
+++ server/src/dbHelperScripts/insertDates.ts
@@ -1,15 +1,20 @@
 import { Database } from '../db/Database'
 import { Inscription } from '../models/Inscription'
+import process from 'process';
 
 let db = new Database();
 
 let proms: Promise<any>[] = []
 
+const startDate = "2022-06-20"
+
+const isWeekend = (day: Number) => day == 0 || day == 6
+
 db.clientPromise().then(() => {
     for (let i = 0; i < 1000; i++) {
-        let date = new Date();
+        let date = new Date(startDate);
         date.setDate(date.getDate() + i);
-        date.setHours(1);
+        date.setHours(12);
         date.setMinutes(0);
         date.setSeconds(0);
         date.setMilliseconds(0);
@@ -17,11 +22,26 @@ db.clientPromise().then(() => {
         let newIns = new Inscription({
             date: date,
             lunch: [],
-            dinner: []
+            dinner: [],
+            noLunch: !isWeekend(date.getDay())
         });
-        proms.push(newIns.save().catch(_ => {}).then(_ => console.log(date)));
+        proms.push(newIns.save().catch(_ => {}).then(_ => {
+            console.log(date.toString() + " " + !isWeekend(date.getDay()));
+        }
+            ));
         // date.setDate(date.getDate() + 1);    
     }
 
-    Promise.all(proms).then(_ => console.log("all done"));
+    Promise.all(proms).then(_ => {
+        console.log("all done");
+        process.exit();
+    });
 });
+
+// 0 Sunday
+// 1 Mon
+// 2 Tues
+// 3 Wed
+// 4 Thurs
+// 5 Fri
+// 6 Sat
